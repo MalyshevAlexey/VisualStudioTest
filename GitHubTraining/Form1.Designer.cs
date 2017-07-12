045ef3c150914dafc95d692e09dc17eb7f52cd73
Alexey Malyshev <alexey.malyshev@hotmail.com>
2017-07-12 13:37:25 +0300

    Alexey Gintaras Merge

--- GitHubTraining/Form1.Designer.cs
+++ GitHubTraining/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.buttonTest = new System.Windows.Forms.Button();
-
             this.buttonAlexey = new System.Windows.Forms.Button();
             this.buttonGintaras = new System.Windows.Forms.Button();
             this.SuspendLayout();
@@ -44,17 +43,16 @@
             this.buttonTest.UseVisualStyleBackColor = true;
             this.buttonTest.Click += new System.EventHandler(this.buttonTest_Click);
             // 
-<<<<<<< HEAD
             // buttonAlexey
             // 
-            this.buttonAlexey.Location = new System.Drawing.Point(28, 70);
+            this.buttonAlexey.Location = new System.Drawing.Point(28, 105);
             this.buttonAlexey.Name = "buttonAlexey";
             this.buttonAlexey.Size = new System.Drawing.Size(75, 26);
             this.buttonAlexey.TabIndex = 1;
             this.buttonAlexey.Text = "Alexey";
             this.buttonAlexey.UseVisualStyleBackColor = true;
             this.buttonAlexey.Click += new System.EventHandler(this.buttonAlexey_Click);
-=======
+            // 
             // buttonGintaras
             // 
             this.buttonGintaras.Location = new System.Drawing.Point(28, 63);
@@ -64,19 +62,14 @@
             this.buttonGintaras.Text = "Gintaras";
             this.buttonGintaras.UseVisualStyleBackColor = true;
             this.buttonGintaras.Click += new System.EventHandler(this.buttonGintaras_Click);
->>>>>>> gintaras
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-<<<<<<< HEAD
-            this.ClientSize = new System.Drawing.Size(284, 262);
-            this.Controls.Add(this.buttonAlexey);
-=======
             this.ClientSize = new System.Drawing.Size(340, 262);
+            this.Controls.Add(this.buttonAlexey);
             this.Controls.Add(this.buttonGintaras);
->>>>>>> gintaras
             this.Controls.Add(this.buttonTest);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
